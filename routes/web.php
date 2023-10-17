b22ec26dbaefc0a46bef657b8dbe01958ce29047
AntLevilloux <145355209+AntLevilloux@users.noreply.github.com>
2023-10-17 10:10:48 +0200

    installation de breeze (je crois)

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
-use App\Http\Controllers\ChampionnatController;
-use App\Http\Controllers\EquipeController;
-use App\Http\Controllers\JoueurController;
-use App\Http\Controllers\MatchController;
-use App\Models\Matche;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,15 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::resource('championnat', ChampionnatController::class);
-
-Route::resource('equipe', EquipeController::class);
-
-Route::resource('joueur', JoueurController::class);
-
-Route::resource('match', MatchController::class);
-
 Route::get('/', function () {
-    return redirect('championnat');
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
